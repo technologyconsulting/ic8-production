3347600465ac86269d230c29327a9f26e98e4d74
simon.edwards <simon.edwards@eastechnologyconsulting.com>
2024-01-14 15:59:59 +0100

    refcator: client portal button width

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -38,7 +38,7 @@ export default function Header() {
         <SignedOut>
           {/* Signed out users get sign in button */}
           <SignInButton mode="modal">
-            <button className="py-1 xl:w-80 xl:py-2">
+            <button className="py-1 xl:w-fit xl:py-2">
               <p className="mb-12 xl:mb-0 text-lg uppercase after:block after:content-[''] after:border-b-2 after:border-b-ic8 after:scale-x-0 after:transform after:duration-200 after:ease-in-out after:py-2 hover:after:scale-x-100 hover:after:py-2 md:mb-0 md:mt-4 xl:pt-6 xl:align-middle">
                 Client Portal
               </p>
